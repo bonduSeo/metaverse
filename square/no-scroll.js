ddc290d15ee1cd6c9b76d81337d733c87a890eee
nearTeacher <bondu.seo@gmail.com>
2022-07-25 18:10:33 +0900

    first commit

---
+++ square/no-scroll.js
@@ -0,0 +1,55 @@
+var map = {
+    cols: 8,
+    rows: 8,
+    tsize: 64,
+    tiles: [
+        1, 3, 3, 3, 1, 1, 3, 1,
+        1, 1, 1, 1, 1, 1, 1, 1,
+        1, 1, 1, 1, 1, 2, 1, 1,
+        1, 1, 1, 1, 1, 1, 1, 1,
+        1, 1, 1, 2, 1, 1, 1, 1,
+        1, 1, 1, 1, 2, 1, 1, 1,
+        1, 1, 1, 1, 2, 1, 1, 1,
+        1, 1, 1, 1, 2, 1, 1, 1
+    ],
+    // col행 row열의 타일값을 반환
+    getTile: function (col, row) {
+        return this.tiles[row * map.cols + col];
+    }
+};
+
+// loader.imges 객체에  {tiles: <img src="~~">} 이렇게 값 삽입
+Game.load = function () {
+    return [
+        Loader.loadImage('tiles', '../assets/tiles.png')
+    ];
+};
+
+Game.init = function () {
+    this.tileAtlas = Loader.getImage('tiles');
+    // =객체 <img src="~~"?
+};
+
+Game.update = function (delta) {
+};
+
+Game.render = function () {
+    for (var c = 0; c < map.cols; c++) {
+        for (var r = 0; r < map.rows; r++) {
+            var tile = map.getTile(c, r);
+            if (tile !== 0) { // 0 => empty tile
+                this.ctx.drawImage(
+                    this.tileAtlas, // image
+                    (tile - 1) * map.tsize, // source x
+                    0, // source y
+                    map.tsize, // source width
+                    map.tsize, // source height
+                    c * map.tsize,  // target x
+                    r * map.tsize, // target y
+                    map.tsize, // target width
+                    map.tsize // target height
+                );
+            }
+        }
+    }
+};
