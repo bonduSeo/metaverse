dcbaf483abe45fe5ee8fa45f561c0b5d525ba4d9
zyakal <pkd1426@gmail.com>
2022-08-10 15:23:40 +0900

    08.10 상호작용 크기 수정

--- js/interactive.js
+++ js/interactive.js
@@ -24,12 +24,12 @@ const base = {
       // 오구 프로젝트
       if (base.interactiveNumber === 12) {
         const modalBody = document.querySelector(".modal-body");
-        modalBody.innerHTML = `<div class="row"><div class="col"></div><div class="col"><iframe id="inlineFrameExample"
-          title="Inline Frame Example"
+        modalBody.innerHTML = `<div class="row"><div class="col-xl-4 col-md-3 col-sm-1"></div><div class="col-xl-4 col-md-6 col-sm-10 col-xs-12"><iframe id="inlineFrameExample"
+          title="Inline Frame Example"     
           width="100%"
-          height="100%"
+          height="100%"     
           src="http://3.39.59.227/home.php">
-          </iframe></div><div class="col"></div></div>`;
+          </iframe></div><div class="col-xl-4 col-md-3 col-sm-1"></div></div>`;
       }
       if (base.interactiveNumber === 13) {
         const modalBody = document.querySelector(".modal-body");
