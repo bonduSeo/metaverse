4ee83295f5323db6ba7646197ca4a7558faf0c6d
bondu <bondu.seo@gmail.com>
2022-08-22 15:31:26 +0900

    for GCP

--- js/interactive_localhost.js
+++ js/interactive_localhost.js
@@ -11,33 +11,35 @@ const base = {
     if (e.code === "Space") {
       document.querySelector(".modalClick").click();
       base.missionCheck(base.interactiveNumber);
-      // 재훈팀
+      // 라네즈
       if (base.interactiveNumber === 11) {
         const modalBody = document.querySelector(".modal-body");
         modalBody.innerHTML = `<iframe id="inlineFrameExample"
           title="Inline Frame Example"
           width="100%"
           height="100%"
-          src="http://13.209.108.252:5000/">
+          src="http://192.168.0.52:8080/">
           </iframe>`;
       }
-      // 오구 프로젝트
+      // 한끼어때
       if (base.interactiveNumber === 12) {
         const modalBody = document.querySelector(".modal-body");
-        modalBody.innerHTML = `<div class="row"><div class="col-xl-4 col-md-3 col-sm-1"></div><div class="col-xl-4 col-md-6 col-sm-10 col-xs-12"><iframe id="inlineFrameExample"
-          title="Inline Frame Example"     
+        modalBody.innerHTML = `<iframe id="inlineFrameExample"
+          title="Inline Frame Example"
           width="100%"
-          height="100%"     
-          src="http://3.39.59.227/home.php">
-          </iframe></div><div class="col-xl-4 col-md-3 col-sm-1"></div></div>`;
+          height="100%"
+          src="http://192.168.0.42:8080/">
+          </iframe>`;
       }
+
+      //모임
       if (base.interactiveNumber === 13) {
         const modalBody = document.querySelector(".modal-body");
         modalBody.innerHTML = `<iframe id="inlineFrameExample"
           title="Inline Frame Example"
           width="100%"
           height="100%"
-          src="http://3.39.59.227/owner_login.php">
+          src="http://192.168.0.71:8080/">
           </iframe>`;
       }
       if (base.interactiveNumber === 14) {
